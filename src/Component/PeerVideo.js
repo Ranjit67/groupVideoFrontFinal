a9309e6e8283f8f63415532c9ed322d927b66ca9
Ranjit67 <58407890+Ranjit67@users.noreply.github.com>
2021-03-17 16:47:22 +0530

    Update PeerVideo.js

--- src/Component/PeerVideo.js
+++ src/Component/PeerVideo.js
@@ -1,11 +1,39 @@
 import { useRef, useEffect, useState } from "react";
 import io from "socket.io-client";
-// import Peer from "simple-peer";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+import { Button } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  peerVideo: {
+    [theme.breakpoints.up("md")]: {
+      width: "360px",
+      margin: "0 10px 15px 0",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      marginBottom: "20px",
+    },
+  },
+  nameHoldDiv: {
+    padding: "0 0 5px 20px",
+  },
+  disconnectBtn: {
+    backgroundColor: "#ff7675",
+    marginBottom: "30px",
+  },
+}));
 const PeerVideo = (props) => {
   const ref = useRef();
   const socketRef = useRef();
   const [clientName, setclientName] = useState();
   const [host, sethost] = useState();
+  const classes = useStyles();
   useEffect(() => {
     socketRef.current = io("https://groupvideocallapi.herokuapp.com/");
     props.peer.on("stream", (stream) => {
@@ -28,21 +56,31 @@ const PeerVideo = (props) => {
 
   return (
     <div style={{ display: "inline-block" }}>
-      <div>
+      {/* name */}
+      <div className={classes.nameHoldDiv}>
         {clientName && <h3>{clientName}</h3>}
         {host && <h1 style={{ color: "red" }}>{host}</h1>}
       </div>
+      {/* video */}
       <div>
-        <video playsInline autoPlay ref={ref} width="360" />
+        <video
+          playsInline
+          autoPlay
+          ref={ref}
+          className={classes.peerVideo}
+          width="360"
+        />
       </div>
+      {/* button */}
       {props.roomName && (
         <div>
-          <button
-            style={{ background: "red", color: "white" }}
+          <Button
+            variant="contained"
+            className={classes.disconnectBtn}
             onClick={props.disconnect}
           >
-            Disconnect{" "}
-          </button>
+            Disconnect
+          </Button>
         </div>
       )}
     </div>
