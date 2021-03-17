8ef4d517ca53412482b6e2e850e80684ba880c4b
Ranjit67 <58407890+Ranjit67@users.noreply.github.com>
2021-03-17 16:49:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 import { v1 as uuid } from "uuid";
 import Room from "./Component/Room";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "40%",
+    width: "100%",
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  createButton: {
+    margin: "0 20px 0 0",
+  },
+}));
 export default function App() {
   const [uui, setuui] = useState("");
   const [forJoin, setforJoin] = useState(true);
@@ -12,15 +27,32 @@ export default function App() {
   const joinRoomHandler = () => {
     setforJoin(false);
   };
+  const cleanUui = () => {
+    setuui();
+  };
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.mainRoot}>
       {!uui && forJoin ? (
-        <div>
-          <button onClick={createRoom}>Create room</button>
-          <button onClick={joinRoomHandler}>Join room</button>
+        <div className={classes.root}>
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.createButton}
+            onClick={createRoom}
+          >
+            Create New Room
+          </Button>
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={joinRoomHandler}
+          >
+            Join Rooms
+          </Button>
         </div>
       ) : null}
-      {uui || !forJoin ? <Room uui={uui} /> : null}
+      {uui || !forJoin ? <Room uui={uui} cleanUui={cleanUui} /> : null}
       {/* <video muted ref={myVideo} autoPlay playsInline /> */}
     </div>
   );
